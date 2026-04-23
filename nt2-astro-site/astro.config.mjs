80eaabecd3ef55b9b3a5ffeed5d26935f14b0764
Jurriaan Tjalsma <jurriaantjalsma@hotmail.com>
2026-04-23 23:15:39 +0200

    online

--- nt2-astro-site/astro.config.mjs
+++ nt2-astro-site/astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://nt2-site.vercel.app' // ⚠️ Vervangen door definitief domein zodra aangekocht
+  site: 'https://taaldiewerkt.nl'
 });
